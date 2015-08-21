3097688a395b7e32eaaee78954de57dfc33740c0
Cillian <mylsey4thewin@gmail.com>
2015-08-21 11:41:43 +0100

    Bash functions and more set up admin.

--- setup.sh
+++ setup.sh
@@ -2,7 +2,6 @@
 sudo apt-get update 
 sudo apt-get upgrade
 
-
 # Various tools, codecs, JRE, JDK
 sudo apt-get install
 	gnome-tweak-tool
@@ -12,26 +11,28 @@ sudo apt-get install
 	openjdk-7-jre
 	openjdk-7-jdk
 
-
 # Google Chrome -- search and follow intructions
 # Chromium Web Browser -- Ubuntu Software Center
 
-
 # Install Java 8 
-$ sudo apt-get update
-$ sudo apt-get install oracle-java8-installer
-$ java -version
-java version "1.8.0_45"
-Java(TM) SE Runtime Environment (build 1.8.0_45-b14)
-Java HotSpot(TM) 64-Bit Server VM (build 25.45-b02, mixed mode)
-$ sudo apt-get install oracle-java8-set-default
-
+sudo apt-get install oracle-java8-installer
+java -version
+# java version "1.8.0_45"
+# Java(TM) SE Runtime Environment (build 1.8.0_45-b14)
+# Java HotSpot(TM) 64-Bit Server VM (build 25.45-b02, mixed mode)
+sudo apt-get install oracle-java8-set-default
 
 # Set up SSH key for GitHub >> Tutorial
 https://help.github.com/articles/generating-ssh-keys/
 
-
-# Install FileZilla
-sudo apt-get update
+# Install FileZilla (FTP
 sudo apt-get install filezilla
 
+# Install Meld (diff visualisation tool) 
+sudo apt-get insall meld
+
+# Insall Pidgin (XMPP client)
+sudo apt-get install pidgin
+
+# Install Marker (screenshot annotation/sharing service)
+https://insync.io/marker # Download
