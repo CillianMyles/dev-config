e1a6182384d686b302bcd9674523b0e62ec3105e
Cillian Myles <mylsey4thewin@gmail.com>
2016-01-14 18:47:47 +0000

    Update and rename setup.sh to initial_setup.sh

--- initial_setup.sh
+++ initial_setup.sh
@@ -1,8 +1,12 @@
-# Update everything
+# Ensure correct updates occur.
+# Add the following line to /etc/apt/sources.list.d/sources.list
+deb http://archive.ubuntu.com/ubuntu trusty main universe restricted multiverse
+
+# Update everything.
 sudo apt-get update 
 sudo apt-get upgrade
 
-# Various tools, codecs, JRE, JDK
+# Various tools, codecs, JRE, JDK.
 sudo apt-get install
 	gnome-tweak-tool
 	ubuntu-restricted-extras
@@ -11,8 +15,8 @@ sudo apt-get install
 	openjdk-7-jre
 	openjdk-7-jdk
 
-# Google Chrome -- search and follow intructions
-# Chromium Web Browser -- Ubuntu Software Center
+# Google Chrome -- search and follow intructions.
+# Chromium Web Browser -- Ubuntu Software Center.
 
 # Install Java 8 
 sudo apt-get install oracle-java8-installer
@@ -22,17 +26,20 @@ java -version
 # Java HotSpot(TM) 64-Bit Server VM (build 25.45-b02, mixed mode)
 sudo apt-get install oracle-java8-set-default
 
+# Install git.
+sudo apt-get install git
+
 # Set up SSH key for GitHub >> Tutorial
 https://help.github.com/articles/generating-ssh-keys/
 
-# Install FileZilla (FTP
+# Install FileZilla (FTP).
 sudo apt-get install filezilla
 
-# Install Meld (diff visualisation tool) 
+# Install Meld (diff visualisation tool). 
 sudo apt-get insall meld
 
-# Insall Pidgin (XMPP client)
+# Insall Pidgin (XMPP client).
 sudo apt-get install pidgin
 
-# Install Marker (screenshot annotation/sharing service)
+# Install Marker (screenshot annotation/sharing service).
 https://insync.io/marker # Download
