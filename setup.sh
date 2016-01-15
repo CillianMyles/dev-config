a1ced12cc8cba8d0726de4c0a6ff684ec2d6f989
Cillian <mylsey4thewin@gmail.com>
2016-01-15 11:29:45 +0000

    Adding some installations.

--- setup.sh
+++ setup.sh
@@ -36,3 +36,12 @@ sudo apt-get install pidgin
 
 # Install Marker (screenshot annotation/sharing service)
 https://insync.io/marker # Download
+
+# Midnight Commander.
+sudo add-apt-repository ppa:eugenesan/ppa
+sudo apt-get update
+sudo apt-get install mc
+
+sudo apt-get install
+  sqlitebrowser
+  keepass2
