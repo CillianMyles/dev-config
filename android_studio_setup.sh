678ab987365f8993aadae858549b801a11d7af45
Cillian <mylsey4thewin@gmail.com>
2016-01-27 09:42:08 +0000

    Updating Android Studio set up & renaming files.

--- android_studio_setup.sh
+++ android_studio_setup.sh
@@ -1,10 +1,10 @@
-# Download the zip from :
+# Download the zip from:
 https://developer.android.com/sdk/installing/index.html?pkg=studio
 
-# Unzip to /opt directory (optional directory)
+# Unzip to /opt directory (optional software directory).
 sudo unzip android-studio-ide-141.1980579-linux.zip -d /opt
 
-# Install 32-bit packages
+# Install 32-bit packages (wasn't needed for most recent installation).
 sudo apt-get install lib32z1 lib32ncurses5 lib32bz2-1.0 lib32stdc++6
 
 # Move to the installation's bin directory
@@ -16,11 +16,11 @@ sh studio.sh
 ## Add Android Studio to PATH
 cd ~
 
-# Copy .bashrc file (as backup)
-cp .bashrc .bashrc.2015.07.30.android-studio
+# Make copy of .bashrc file (not necessary step).
+cp .bashrc .bashrc.YYYY.MM.DD.android-studio
 
 # Edit the bashrc file to add Studio's bin folder to PATH
-nano .bashrc ... or ... subl .bashrc
+subl .bashrc # You can edit the file however you like e.g. nano
 
 # Add the Android Studio /bin directory to PATH
 export PATH="/opt/android-studio/bin/:$PATH"
