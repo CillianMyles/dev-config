4d1e7251326eba2fdf2928cf3b11eda0eed4fec2
Cillian <mylsey4thewin@gmail.com>
2016-02-05 15:11:38 +0000

    How to copy marker files & install on another machine (since insync took it down).

---
+++ marker-copy-instructions.sh
@@ -0,0 +1,37 @@
+# Run the following command to create a text file 
+dpkg -L marker >> ~/Desktop/marker-files.txt
+
+# Create a file on the desktop called marker.sh and paste the following:
+#!/bin/bash
+TARGET_DIR="/home/cillian/Desktop/marker-temp/"
+FILES_LIST="/home/cillian/Desktop/marker-files.txt"
+
+for source in `cat $FILES_LIST`
+do
+  echo $source
+  if [ -f $source ]
+  then 
+    mkdir -p $TARGET_DIR`dirname $source` 
+    cp $source $TARGET_DIR$source
+  fi 
+done
+
+# Run the script.
+./Desktop/marker.sh
+
+# Zip up the folder into a archive.
+cd ~/Desktop/marker-temp/ && tar -cvf ../marker.tgz *
+
+# Copy this file to USB or similar.
+
+# When on secondary machine you wish to install marker on:
+cp /path/to/marker.tgz /
+
+# Unzip the files.
+sudo tar -xvf marker.tgz
+
+# Make sure all symlinks correct.
+sudo ldconfig
+
+# Test.
+marker start
