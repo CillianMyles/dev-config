c586b879ead3ff8c7e2995acf1cb05fa657afb1b
Cillian <mylsey4thewin@gmail.com>
2016-02-16 14:38:53 +0000

    Launching at startup.

--- screensaver.sh
+++ screensaver.sh
@@ -3,3 +3,6 @@ sudo apt-get install xscreensaver xscreensaver-data-extra xscreensaver-gl-extra
 
 # Install BSOD.
 sudo apt-get install xscreensaver-screensaver-bsod
+
+# Launch at startup - add the following commands to Startup Applications:
+xscreensaver -nosplash
