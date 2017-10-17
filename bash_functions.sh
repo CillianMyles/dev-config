3a61e680e80b65492c2a952a8d0b0f7c93d48ee7
Cillian Myles <mylsey4thewin@gmail.com>
2017-10-17 09:56:48 +0100

    Update bash rc file.

--- bash_functions.sh
+++ bash_functions.sh
@@ -1,9 +1,10 @@
+
 # Add custom functions to path.
 if [ -f ~/drive/work/dev/scripts/bash/bash_functions.sh ]; then
     . ~/drive/work/dev/scripts/bash/bash_functions.sh
 fi
 
-# Sync Google Drive folders
+# Sync Google Drive folders.
 function sync() {
     old_dir=$PWD
 
@@ -19,3 +20,26 @@ function sync() {
 
     cd $old_dir
 }
+
+# Environment variables & PATH.
+export JAVA_7="/usr/lib/jvm/java-7-openjdk-amd64"
+export JAVA_8="/usr/lib/jvm/java-8-oracle"
+export ANDROID_HOME="/home/cillian/Android/Sdk"
+export STUDIO_HOME="/opt/android-studio/2.3"
+export IDEA_HOME="/opt/idea/2017.2"
+export GRADLE_HOME="/opt/gradle/2.12"
+export QT_HOME="/opt/qt/5.7/gcc_64"
+export JAVA_HOME="$JAVA_8"
+export JDK_HOME="$JAVA_HOME"
+export STUDIO_JDK="$JAVA_7"
+
+export PATH="$STUDIO_HOME/bin/:$PATH"
+export PATH="$ANDROID_HOME/tools/:$PATH"
+export PATH="$ANDROID_HOME/platform-tools/:$PATH"
+export PATH="$GRADLE_HOME/bin/:$PATH"
+export PATH="$QT_HOME/bin/:$PATH"
+
+alias astudio="$STUDIO_HOME/bin/studio.sh"
+alias idea="$IDEA_HOME/bin/idea.sh"
+
+export PATH="$PATH:$HOME/.rvm/bin" # NB - keep this at the bottom!!
