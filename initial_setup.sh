682406e34a59f64f05da6e572172a82efbb29726
Cillian Myles <mylsey4thewin@gmail.com>
2018-05-30 11:05:29 +0100

    Update initial config instructions;

--- initial_setup.sh
+++ initial_setup.sh
@@ -1,39 +1,29 @@
-# Don't include online search results in unity search
-System Settings > Security & Privacy > Search
-
-# Ensure correct updates occur.
-# Add the following line to /etc/apt/sources.list.d/sources.list
-deb http://archive.ubuntu.com/ubuntu trusty main universe restricted multiverse
-
 # Update everything.
 sudo apt-get update 
 sudo apt-get upgrade
+sudo apt-get dist-upgrade
+sudo apt-get autoremove
 
-# Menu options in title bar
+# GNOME tweak tools.
 sudo apt-get install gnome-tweak-tool
-gnome-tweak-tool
-# Typing > enable kill the X server by ctrl-alt-backspace
-# system settings > appaearance > show the menus for a window
-# > in the window's title bar
+gnome-tweaks
+# Make any changes to GNOME you like :-)
 
-# Various tools, codecs, JRE, JDK.
-sudo apt-get install ubuntu-restricted-extras libavcodec-extra icedtea-plugin openjdk-7-jre openjdk-7-jdk
+# Media codecs.
+sudo apt-get install ubuntu-restricted-extras libavcodec-extra
 
-# Google Chrome -- search and follow intructions.
-# Chromium Web Browser -- Ubuntu Software Center.
-
-# Install Java 8 
-sudo add-apt-repository ppa:webupd8team/java
-sudo apt-get update
-sudo apt-get install oracle-java8-installer
+# Java 8.
+sudo apt-get install openjdk-8-jre openjdk-8-jdk
 java -version
-# java version "1.8.0_45"
-# Java(TM) SE Runtime Environment (build 1.8.0_45-b14)
-# Java HotSpot(TM) 64-Bit Server VM (build 25.45-b02, mixed mode)
-sudo apt-get install oracle-java8-set-default
+# java version "1.8.0_XYZ"
+# Java(TM) SE Runtime Environment (build 1.8.0_XYZ)
+# Java HotSpot(TM) 64-Bit Server VM (build XYZ, mixed mode)
 
 # Install some essential packages.
 sudo apt-get install git mc keepass2 gimp shutter filezilla wireshark meld pidgin
+
+# Some more (using snap format).
+snap install postman-snaped
 
 # Install flux (brightness tool) - install instructions:
 https://github.com/xflux-gui/fluxgui
@@ -46,14 +36,12 @@ sudo apt-get install sqlitebrowser
 # Set up SSH key for GitHub >> Tutorial
 https://help.github.com/articles/generating-ssh-keys/
 
-# TODO: Un-install newtwork manager
-# TODO: Install wicd
+# Hardware specific.
+sudo apt-get install solaar
 
-# TODO: Install firefox developer edition
-
-# TODO: Install qt
-
-# TODO: Install gradle
+# Google Chrome -- web search.
+# Franz -- web search.
+# Gradle -- web search.
 
 ## DEPRECATED (all below)
 
