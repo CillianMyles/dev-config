77a1504613dd73417d042637f6fe175db9f44a29
Cillian Myles <mylsey4thewin@gmail.com>
2019-03-07 09:45:02 +0100

    Adding aliases from separate file for git commands; env. vars;

--- bash_rc.sh
+++ bash_rc.sh
@@ -4,6 +4,11 @@ if [ -f ~/drive/work/dev/scripts/bash/bash_functions.sh ]; then
     . ~/drive/work/dev/scripts/bash/bash_functions.sh
 fi
 
+# Add custom aliases to path.
+if [ -f ~/drive/work/dev/scripts/bash/bash_aliases.sh ]; then
+    . ~/drive/work/dev/scripts/bash/bash_aliases.sh
+fi
+
 # Sync Google Drive folders.
 function sync() {
     old_dir=$PWD
@@ -27,10 +32,12 @@ export JAVA_8="/usr/lib/jvm/java-8-openjdk-amd64"
 export ANDROID_HOME="/home/cillian/Android/Sdk"
 export STUDIO_HOME="/snap/android-studio/current/android-studio"
 export IDEA_HOME="/snap/intellij-idea-community/current"
-export GRADLE_HOME="/opt/gradle/4.4"
+export GRADLE_HOME="/opt/gradle/4.6"
 export JAVA_HOME="$JAVA_8"
 export JDK_HOME="$JAVA_HOME"
 export STUDIO_JDK="$JAVA_8"
+export HEIMDALL_HOME="/opt/Heimdall"
+export FLUTTER_HOME="/opt/flutter/1.0.0"
 
 # Path.
 export PATH="$JAVA_HOME/bin/:$PATH"
@@ -38,6 +45,9 @@ export PATH="$STUDIO_HOME/bin/:$PATH"
 export PATH="$ANDROID_HOME/tools/:$PATH"
 export PATH="$ANDROID_HOME/platform-tools/:$PATH"
 export PATH="$GRADLE_HOME/bin/:$PATH"
+export PATH="$HOME/.rbenv/bin:$PATH"
+export PATH="$HEIMDALL_HOME/build/bin:$PATH"
+export PATH="$FLUTTER_HOME/bin:$PATH"
 
 # Aliases.
 alias astudio="$STUDIO_HOME/bin/studio.sh"
