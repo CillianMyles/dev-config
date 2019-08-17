8d64768eeb135199419c662a107eaee7f55d36f0
Cillian Myles <mylsey4thewin@gmail.com>
2019-08-17 08:28:40 +0200

    Adding Google Play Music Desktop Player details

---
+++ google-play-music.sh
@@ -0,0 +1,23 @@
+# Install using snap
+sudo snap install google-play-music-desktop-player
+
+# More info
+https://github.com/MarshallOfSound/Google-Play-Music-Desktop-Player-UNOFFICIAL-
+
+# Manual deb/Ubuntu install
+https://github.com/MarshallOfSound/Google-Play-Music-Desktop-Player-UNOFFICIAL-/blob/master/docs/Installation_On_Debian.md
+
+# Themes
+https://github.com/MarshallOfSound/Google-Play-Music-Desktop-Player-UNOFFICIAL-/wiki/Themes
+
+# Fix for Media key controls
+https://github.com/MarshallOfSound/Google-Play-Music-Desktop-Player-UNOFFICIAL-/wiki/FAQ#my-media-keys-dont-work
+
+# Enable YouTube Music
+https://github.com/MarshallOfSound/Google-Play-Music-Desktop-Player-UNOFFICIAL-/wiki/Enabling-YouTube-Music-under-Linux
+
+# Fix Snap icon in desktop entry
+sudo nano /var/lib/snapd/desktop/applications/google-play-music-desktop-player_google-play-music-desktop-player.desktop
+
+# Update the line
+Icon=/snap/google-play-music-desktop-player/current/meta/gui/icon.png
