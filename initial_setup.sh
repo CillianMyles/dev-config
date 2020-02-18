5d0e5440b7640b25ab2b5f23fcf28c758666c5fd
Cillian Myles <mylsey4thewin@gmail.com>
2020-02-18 11:41:27 +0000

    Add KeePassXC dependency; swap 'apt-get' -> 'apt';

--- initial_setup.sh
+++ initial_setup.sh
@@ -1,43 +1,43 @@
 # Update everything.
-sudo apt-get update 
-sudo apt-get upgrade
-sudo apt-get dist-upgrade
-sudo apt-get autoremove
+sudo apt update 
+sudo apt upgrade
+sudo apt dist-upgrade
+sudo apt autoremove
 
 # GNOME tweak tools.
-sudo apt-get install gnome-tweak-tool
+sudo apt install gnome-tweak-tool
 gnome-tweaks
 # Make any changes to GNOME you like :-)
 
 # Media codecs.
-sudo apt-get install ubuntu-restricted-extras libavcodec-extra
+sudo apt install ubuntu-restricted-extras libavcodec-extra
 
 # Java 8.
-sudo apt-get install openjdk-8-jre openjdk-8-jdk
+sudo apt install openjdk-8-jre openjdk-8-jdk
 java -version
 # java version "1.8.0_XYZ"
 # Java(TM) SE Runtime Environment (build 1.8.0_XYZ)
 # Java HotSpot(TM) 64-Bit Server VM (build XYZ, mixed mode)
 
 # Install some essential packages.
-sudo apt-get install git mc keepass2 gimp shutter filezilla wireshark meld pidgin tree
+sudo apt install git mc keepass2 gimp shutter filezilla wireshark meld pidgin tree
 
 # Some more (using snap format).
-snap install postman-snaped tldr
+sudo snap install postman-snaped tldr keepassxc
 
 # Install flux (brightness tool) - install instructions:
 https://github.com/xflux-gui/fluxgui
 
 # Install SQLiteBrowser tool.
 sudo add-apt-repository ppa:linuxgndu/sqlitebrowser
-sudo apt-get update
-sudo apt-get install sqlitebrowser
+sudo apt update
+sudo apt install sqlitebrowser
 
 # Set up SSH key for GitHub >> Tutorial
 https://help.github.com/articles/generating-ssh-keys/
 
 # Hardware specific.
-sudo apt-get install solaar
+sudo apt install solaar
 
 # Google Chrome -- web search.
 # Franz -- web search.
