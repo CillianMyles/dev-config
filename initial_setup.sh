dcad3f1da079649560b51e7d37144a3ca12b7b0b
Cillian Myles <mylsey4thewin@gmail.com>
2020-02-18 13:49:02 +0000

    Peek (screen recording) tool; tidy up;

--- initial_setup.sh
+++ initial_setup.sh
@@ -1,47 +1,52 @@
-# Update everything.
+# Update everything
 sudo apt update 
 sudo apt upgrade
 sudo apt dist-upgrade
 sudo apt autoremove
 
-# GNOME tweak tools.
+# GNOME tweak tools
 sudo apt install gnome-tweak-tool
 gnome-tweaks
 # Make any changes to GNOME you like :-)
 
-# Media codecs.
+# Media codecs
 sudo apt install ubuntu-restricted-extras libavcodec-extra
 
-# Java 8.
+# Java 8
 sudo apt install openjdk-8-jre openjdk-8-jdk
 java -version
 # java version "1.8.0_XYZ"
 # Java(TM) SE Runtime Environment (build 1.8.0_XYZ)
 # Java HotSpot(TM) 64-Bit Server VM (build XYZ, mixed mode)
 
-# Install some essential packages.
+# Install some essential packages
 sudo apt install git mc keepass2 gimp shutter filezilla wireshark meld pidgin tree
 
-# Some more (using snap format).
+# Some more (using snap format)
 sudo snap install postman-snaped tldr keepassxc
 
 # Install flux (brightness tool) - install instructions:
 https://github.com/xflux-gui/fluxgui
 
-# Install SQLiteBrowser tool.
+# Install SQLiteBrowser tool
 sudo add-apt-repository ppa:linuxgndu/sqlitebrowser
 sudo apt update
 sudo apt install sqlitebrowser
 
+# Install Peek (screen recorder) tool
+sudo add-apt-repository ppa:peek-developers/stable
+sudo apt update
+sudo apt install peek
+
 # Set up SSH key for GitHub >> Tutorial
 https://help.github.com/articles/generating-ssh-keys/
 
-# Hardware specific.
+# Hardware specific
 sudo apt install solaar
 
-# Google Chrome -- web search.
-# Franz -- web search.
-# Gradle -- web search.
+# Google Chrome -- web search
+# Franz -- web search
+# Gradle -- web search
 
 ## DEPRECATED (all below)
 
