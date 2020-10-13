6e9e265dd82a538e296c081484f836bb7c696c02
Cillian Myles <myles.cillian@gmail.com>
2020-10-13 20:16:58 +0100

    sample flutter commands;

---
+++ scripts/flutter_commands.sh
@@ -0,0 +1,23 @@
+# check install state
+flutter doctor
+
+# check version
+flutter --version
+
+# get some help
+flutter help
+
+# list subcommands
+flutter <command> -h
+
+# build all variants
+flutter build
+
+# build for macOS
+flutter build macos
+
+# run on attached device
+flutter run
+
+# run on macOS
+flutter run -d macos
