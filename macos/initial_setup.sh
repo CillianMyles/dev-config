1fc0a3a9a94dccfe3de22046ebf5e85a789aefc8
Cillian Myles <myles.cillian@gmail.com>
2020-10-13 20:55:11 +0100

    updating MacOS initial setup;

--- macos/initial_setup.sh
+++ macos/initial_setup.sh
@@ -1,5 +1,9 @@
 # log in with Apple ID
-# restore from Time Machine backup
+
+# (optional) restore from Time Machine backup
+
 # enable Time Machine backup
 
 # add contents of rc.sh to bottom of .profile .bashrc .zshrc files
+
+# install essential packages e.g. git
