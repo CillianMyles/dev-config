831b138559f7f007364aa98cc8cbea3d1521c02a
Cillian Myles <myles.cillian@gmail.com>
2020-10-16 13:32:58 +0100

    remove un-needed aliases since using oh-myzsh;

--- macos/rc_exports.sh
+++ macos/rc_exports.sh
@@ -1,13 +1,13 @@
 
 # environment variables
-export JAVA_HOME="/Library/Java/JavaVirtualMachines/jdk1.8.0_261.jdk/Contents/Home"
+export JAVA_HOME="/Applications/Android Studio.app/Contents/jre/jdk/Contents/Home"
 export ANDROID_HOME="/Users/cillian/Library/Android/sdk"
 export FLUTTER_HOME="$HOME/git/flutter/google/flutter"
 export GO_HOME="/usr/local/go"
 
 # path
 export PATH="$PATH:$JAVA_HOME/bin"
-export PATH="$ANDROID_HOME/emulator/:$PATH"
-export PATH="$ANDROID_HOME/platform-tools/:$PATH"
+export PATH="$PATH:$ANDROID_HOME/emulator"
+export PATH="$PATH:$ANDROID_HOME/platform-tools"
 export PATH="$PATH:$FLUTTER_HOME/bin"
 export PATH="$PATH:$GO_HOME/bin"
--- macos/rc_setup.sh
+++ macos/rc_setup.sh
@@ -4,12 +4,12 @@ if [ -f ~/git/config/personal/dev-config/macos/rc_exports.sh ]; then
     . ~/git/config/personal/dev-config/macos/rc_exports.sh
 fi
 
-# add aliases
-if [ -f ~/git/config/personal/dev-config/common/bash_aliases.sh ]; then
-    . ~/git/config/personal/dev-config/common/bash_aliases.sh
-fi
-
 # add android functions
 if [ -f ~/git/config/personal/dev-config/scripts/android_functions.sh ]; then
     . ~/git/config/personal/dev-config/scripts/android_functions.sh
 fi
+
+# add aliases -> NO LONGER NEEDED -> since using -> Oh-My-ZSH!
+#if [ -f ~/git/config/personal/dev-config/common/bash_aliases.sh ]; then
+#    . ~/git/config/personal/dev-config/common/bash_aliases.sh
+#fi
