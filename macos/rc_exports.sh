3b61c0589a3e55ce2837b100995478c804edb329
Cillian Myles <myles.cillian@gmail.com>
2020-10-26 17:54:58 +0000

    add dart config for non-futter projects;

--- macos/rc_exports.sh
+++ macos/rc_exports.sh
@@ -3,6 +3,7 @@
 export JAVA_HOME="/Applications/Android Studio.app/Contents/jre/jdk/Contents/Home"
 export ANDROID_HOME="/Users/cillian/Library/Android/sdk"
 export FLUTTER_HOME="$HOME/git/flutter/google/flutter"
+export DART_HOME="/usr/local/Cellar/dart/2.10.2"
 export GO_HOME="/usr/local/go"
 
 # path
@@ -10,4 +11,5 @@ export PATH="$PATH:$JAVA_HOME/bin"
 export PATH="$PATH:$ANDROID_HOME/emulator"
 export PATH="$PATH:$ANDROID_HOME/platform-tools"
 export PATH="$PATH:$FLUTTER_HOME/bin"
+export PATH="$PATH:$DART_HOME/bin"
 export PATH="$PATH:$GO_HOME/bin"
