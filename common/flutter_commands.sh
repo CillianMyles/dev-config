7584aeb207a1bfdc6edb542624a00cd837f76b49
Cillian Myles <myles.cillian@gmail.com>
2021-02-18 19:14:48 +0000

    Add most commonly used flutter commands (possible aliases);

--- common/flutter_commands.sh
+++ common/flutter_commands.sh
@@ -1,7 +1,25 @@
-# check install state
-flutter doctor
+# commonly used -> create aliases (?)
+flutter analyze
+flutter test
+flutter test --coverage
+flutter format .
+flutter format -n --set-exit-if-changed .
+flutter packages pub publish --dry-run
+flutter packages pub publish
 
-# check version
+# code gen -> freezed
+flutter packages pub run build_runner build
+flutter packages pub run build_runner watch
+flutter packages pub run build_runner build --delete-conflicting-outputs
+flutter packages pub run build_runner watch --delete-conflicting-outputs
+
+# debug tools
+remotedev --port 8000 # -> http://localhost:8000
+
+# check install state
+flutter doctor -v
+
+# check flutter version
 flutter --version
 
 # get some help
