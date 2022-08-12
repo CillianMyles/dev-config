ccd630de9b5e08448b9217ec66d242f22cd5d7e4
Cillian Myles <myles.cillian@gmail.com>
2022-08-12 15:52:38 +0100

    update macos apps script;

--- macos/setup_07_apps.sh
+++ macos/setup_07_apps.sh
@@ -2,21 +2,37 @@
 ### via homebrew ###
 ####################
 
-brew cask install intellij-idea-ce
+brew install --cask bitwarden
+brew install --cask brave-browser
+brew install --cask firefox
+brew install --cask dropbox
+brew install --cask notion
+brew install --cask slack
+brew install --cask zoom
+brew install --cask loom
 brew install --cask keycastr
+brew install --cask raycast
+brew install --cask marta
+brew install --cask gimp
+brew install --cask vlc
+
+brew install --cask sublime-text
+brew install --cask visual-studio-code
+brew install --cask intellij-idea-ce
+brew install --cask android-studio
+brew install --cask postman
+
+brew install --cask signal
+brew install --cask whatsapp
+brew install --cask telegram
+
 
 #####################
 ### via app store ###
 #####################
 
-# bitwarden -> https://apps.apple.com/ie/app/bitwarden/id1352778147?mt=12
-# todoist -> https://apps.apple.com/ie/app/todoist-to-do-list-tasks/id585829637?mt=12 
-# trello -> https://apps.apple.com/ie/app/trello/id1278508951?mt=12
-# evernote -> https://apps.apple.com/ie/app/evernote/id406056744?mt=12
-# slack -> https://apps.apple.com/ie/app/slack/id803453959?mt=12
 # xcode -> https://apps.apple.com/ie/app/xcode/id497799835?mt=12
-# whatsapp -> https://apps.apple.com/ie/app/whatsapp-desktop/id1147396723?mt=12 
-# telegram -> https://apps.apple.com/ie/app/telegram/id747648890?mt=12
+# magnet -> https://apps.apple.com/ie/app/magnet/id441258766?mt=12
 # twitter -> https://apps.apple.com/ie/app/twitter/id1482454543?mt=12
 # filezilla -> https://apps.apple.com/ie/app/filezilla-pro-ftp-and-cloud/id1298486723?mt=12 
 
@@ -25,22 +41,10 @@ brew install --cask keycastr
 ### from browser ###
 ####################
 
-# firefox -> https://www.mozilla.org/en-US/firefox/mac 
-# dropbox -> https://www.dropbox.com/en_GB/downloading
 # chrome -> https://www.google.com/chrome
 # drive -> https://www.google.com/intl/en_fj/drive/download 
-# zoom -> https://zoom.us/download
-# signal -> https://signal.org/en/download
-# android studio -> https://developer.android.com/studio
-# sublime text -> https://www.sublimetext.com/3
-# vs code -> https://code.visualstudio.com/download
-# postman -> https://www.postman.com/downloads 
-# notion -> https://www.notion.so/desktop
 # linear -> https://linear.app/download
-# loom -> https://www.loom.com/desktop
-# gimp -> https://www.gimp.org/downloads
 # logitech -> https://www.logitech.com/en-us/product/options
-# vlc -> https://www.videolan.org/vlc/index.html
 
 
 ###############
@@ -48,4 +52,3 @@ brew install --cask keycastr
 ###############
 
 # boop -> https://apps.apple.com/ie/app/boop/id1518425043?mt=12
-# magnet -> https://apps.apple.com/ie/app/magnet/id441258766?mt=12
