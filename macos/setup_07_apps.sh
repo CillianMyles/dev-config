43ccef711e9b156d2a502b9113e5746bce1de0e8
Cillian Myles <myles.cillian@gmail.com>
2023-04-06 11:09:18 +0100

    chore: ensure apps list is up to date;

--- macos/setup_07_apps.sh
+++ macos/setup_07_apps.sh
@@ -4,7 +4,6 @@
 
 brew install --cask bitwarden
 brew install --cask brave-browser
-brew install --cask firefox
 brew install --cask dropbox
 brew install --cask notion
 brew install --cask slack
@@ -18,13 +17,10 @@ brew install --cask vlc
 
 brew install --cask sublime-text
 brew install --cask visual-studio-code
-brew install --cask intellij-idea-ce
-brew install --cask android-studio
 brew install --cask postman
 
 brew install --cask signal
 brew install --cask whatsapp
-brew install --cask telegram
 
 
 #####################
@@ -33,8 +29,6 @@ brew install --cask telegram
 
 # xcode -> https://apps.apple.com/ie/app/xcode/id497799835?mt=12
 # magnet -> https://apps.apple.com/ie/app/magnet/id441258766?mt=12
-# twitter -> https://apps.apple.com/ie/app/twitter/id1482454543?mt=12
-# filezilla -> https://apps.apple.com/ie/app/filezilla-pro-ftp-and-cloud/id1298486723?mt=12 
 
 
 ####################
@@ -44,7 +38,9 @@ brew install --cask telegram
 # chrome -> https://www.google.com/chrome
 # drive -> https://www.google.com/intl/en_fj/drive/download 
 # linear -> https://linear.app/download
-# logitech -> https://www.logitech.com/en-us/product/options
+# logitech -> https://www.logitech.com/en-eu/software/logi-options-plus.html
+# setapp -> https://my.setapp.com
+# jetbrains toolbox ->
 
 
 ###############
@@ -52,3 +48,15 @@ brew install --cask telegram
 ###############
 
 # boop -> https://apps.apple.com/ie/app/boop/id1518425043?mt=12
+
+
+###############
+### archive ###
+###############
+
+# twitter -> https://apps.apple.com/ie/app/twitter/id1482454543?mt=12
+# filezilla -> https://apps.apple.com/ie/app/filezilla-pro-ftp-and-cloud/id1298486723?mt=12
+brew install --cask firefox
+brew install --cask intellij-idea-ce
+brew install --cask android-studio
+brew install --cask telegram
