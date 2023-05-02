127accb21f85267d749e9d392f77fdf474d08fda
Cillian Myles <myles.cillian@gmail.com>
2023-05-02 14:53:28 +0100

    feat: open JDK 11;

--- macos/rc_exports.sh
+++ macos/rc_exports.sh
@@ -1,6 +1,7 @@
 # environment variables
 export JAVA_OPEN_JDK="/opt/homebrew/opt/openjdk"
-export JAVA_HOME="$JAVA_OPEN_JDK"
+export JAVA_11="/opt/homebrew/opt/openjdk@11"
+export JAVA_HOME="$JAVA_11"
 export FLUTTER_HOME="$HOME/git/flutter/google/flutter"
 export PUB_CACHE_HOME="$HOME/.pub-cache"
 
--- macos/setup_09_java.sh
+++ macos/setup_09_java.sh
@@ -1,7 +1,9 @@
-# install openjdk
+# install openjdk, openjdk@11
 brew install openjdk
+brew install openjdk@11
 
 # update rc_exports.sh
 export JAVA_OPEN_JDK="/opt/homebrew/opt/openjdk"
-export JAVA_HOME="$JAVA_OPEN_JDK"
+export JAVA_11="/opt/homebrew/opt/openjdk@11"
+export JAVA_HOME="$JAVA_11"
 export PATH="$PATH:$JAVA_HOME/bin"
