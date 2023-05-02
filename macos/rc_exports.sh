e77e29b42128768e1fd61521ce44cffd0da98079
Cillian Myles <myles.cillian@gmail.com>
2023-05-02 15:18:16 +0100

    chore: exports;

--- macos/rc_exports.sh
+++ macos/rc_exports.sh
@@ -2,9 +2,13 @@
 export JAVA_OPEN_JDK="/opt/homebrew/opt/openjdk"
 export JAVA_11="/opt/homebrew/opt/openjdk@11"
 export JAVA_HOME="$JAVA_11"
+export IDEA_HOME="Applications/JetBrains Toolbox/IntelliJ IDEA Community Edition.app/Contents"
+export IDEA_JDK="$JAVA_HOME"
 export FLUTTER_HOME="$HOME/git/flutter/google/flutter"
 export PUB_CACHE_HOME="$HOME/.pub-cache"
 export ANDROID_HOME="/Users/cillian/Library/Android/sdk"
+export STUDIO_HOME="Applications/JetBrains Toolbox/Android Studio.app/Contents"
+export STUDIO_JDK="$JAVA_HOME"
 
 # path
 export PATH="$PATH:$JAVA_HOME/bin"
