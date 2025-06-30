cc8450edd5b2233e8f81725e201a69290c1d9533
Cillian Myles <myles.cillian@gmail.com>
2025-06-30 07:04:16 +0100

    chore: Tidy up

--- ubuntu_24.04/install.sh
+++ ubuntu_24.04/install.sh
@@ -4,7 +4,7 @@ sudo apt update && sudo apt -y upgrade
 
 ROOT_DIR="https://raw.githubusercontent.com/CillianMyles/dev-config/main"
 FLAVOUR_DIR="$ROOT_DIR/ubuntu_24.04"
-INSTALL_DIR="$FLAVOUR_DIR/install"
+DIR="$FLAVOUR_DIR/install"
 
-source <(wget -qO- "$INSTALL_DIR/app-bitwarden.sh")
-source <(wget -qO- "$INSTALL_DIR/tool-flutter.sh")
+source <(wget -qO- "$DIR/app-bitwarden.sh")
+source <(wget -qO- "$DIR/tool-flutter.sh")
--- ubuntu_24.04/uninstall.sh
+++ ubuntu_24.04/uninstall.sh
@@ -2,7 +2,7 @@
 
 ROOT_DIR="https://raw.githubusercontent.com/CillianMyles/dev-config/main"
 FLAVOUR_DIR="$ROOT_DIR/ubuntu_24.04"
-UNINSTALL_DIR="$FLAVOUR_DIR/install"
+DIR="$FLAVOUR_DIR/uninstall"
 
-source <(wget -qO- "$UNINSTALL_DIR/app-bitwarden.sh")
-source <(wget -qO- "$UNINSTALL_DIR/tool-flutter.sh")
+source <(wget -qO- "$DIR/app-bitwarden.sh")
+source <(wget -qO- "$DIR/tool-flutter.sh")
